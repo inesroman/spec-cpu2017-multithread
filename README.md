2d04712a570e1b616ab46914011192cd0f035ba0
ines <116977832+inesroman@users.noreply.github.com>
2025-03-29 19:55:09 +0100

    Speedup gcc peak

--- README.md
+++ README.md
@@ -1,31 +1,39 @@
 # Primera ejecución: speedup básico 
-He ejecutado el único benchmark de intspeed que es paralelizable con OMP y todos los de fpspeed (que son paralelizables con OMP) menos 627.cam4_s porque da segmentation fault y he visto en varios artículos que les ha pasado lo mismo.
+Este repositorio contiene todos los scripts de `/home/ines/v1_1_9` y sus resultados. Además, `/home/ines/v1_1_9` también contiene los ejecutables e inputs de los benchmarks.
 
-Los benchmarks de fprate y intrate no los he compilado ni ejecutado en esta versión de SPEC CPU2017.
+He ejecutado el único benchmark de intspeed que es paralelizable con OMP (657.xz_s) y todos los de fpspeed (que son paralelizables con OMP), menos 627.cam4_s porque da segmentation fault y he visto en varios artículos que les ha pasado lo mismo.
+
+En esta versión de SPEC CPU2017, no he compilado ni ejecutado los benchmarks de fprate e intrate.
 
 ## 1. Compilación 
 La instalación de SPEC CPU2017 se encuentra en mi home, en `/home/ines/cpu2017`.
 
-He usado el fichero de configuración que viene con spec llamado `Example-gcc-linux-x86.cfg`.
-Lo he copiado en `gcc.cfg` y le he cambiado el flag `-std=c++09` por `-std=c++14` y he añadido `-fallow-argument-mismatch`. No sé si estos son realmente necesarios, pero los añadí porque en la
-versión v1.0.9 de SPEC CPU2017 sí que hacían falta.
+### GCC
+Para la versión compilada con gcc, he usado el fichero de configuración que viene con SPEC llamado `Example-gcc-linux-x86.cfg`. Lo he copiado en `gcc.cfg` y le he cambiado el flag `-std=c++09` por `-std=c++14` y he añadido `-fallow-argument-mismatch`. No sé si estos son realmente necesarios, pero los añadí porque en la versión v1.0.9 de SPEC CPU2017 sí que lo eran. 
+
+Esta configuración ha fallado en la compilación de tres aplicaciones en la versión peak, por lo que he creado el fichero de configuración `peak.cfg`, en el que he cambiado el flag `Ofast` por `O3`.
+
+### AOCC
+Para la versión compilada con aocc,  he usado el fichero de configuración que viene con SPEC llamado `Example-aocc-linux-x86.cfg` y  lo he copiado en `aocc.cfg`. La compilación y ejecución con esta configuración han funcionado para todas las aplicaciones excepto 621.wrf_s. Para esta, he tenido que eliminar el flag `-flto` y he guardado esta nueva configuración en `aocc_621.cfg`.
 
 ## 2. Ejecución
-Este repositorio contiene todos los scripts de `/home/ines/v1_1_9` y sus resultados. Además, `/home/ines/v1_1_9` contiene los ejecutables e inputs de todos los benchmarks de fpspeed y de la aplicación 657.xz_s de intspeed.
+Este repositorio contiene todos los scripts de `/home/ines/v1_1_9` y sus resultados. Además, `/home/ines/v1_1_9` contiene los ejecutables e inputs de las aplicaciones.
 
 ### Setting up
-1. Turbo boost. He apagado el precision boost para que no haga overcloking antes de ejecutar:
+1. Turbo boost. He apagado el Precision Boost para que no haga overclocking antes de ejecutar:
 ```
 echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost
 ```
-2. Screen. He utilizado screen para poder ejecutar el script y que se mantengan los procesos en ejecución aunque cierre la terminal.
+2. Screen. He utilizado Screen para poder ejecutar el script y que se mantengan los procesos en ejecución aunque cierre la terminal.
 
 3. Stack ilimitado. He ejecutado el siguiente comando antes de ejecutar el script para que el stack no tenga límite: 
 ```
 ulimit -s unlimited
 ```
 
-### Resultados
-De cada benchmark he ejecutado una sola copia con 1, 2, 4, ... y 128 threads. Los únicos benchmarks que no he podido ejecutar de esta manera han sido el segundo de la aplicación 657.xz_s (que solo llega hasta 15 threads) y 654.srom_s (solo llega hasta 8). 
+4. Multithreading. SMT está activado y divide los recursos dinámicamente entre los dos hilos de cada núcleo. Las parejas de hilos en el mismo núcleo son (0,64), (1,65), ..., (63,127). Por lo tanto, hasta la ejecución con 128 hilos, no hay hilos compartiendo un mismo núcleo.
 
-Las ejecuciones han sido medidas con perf (por ahora solo ciclos e intrucciones) y los resultados se encuentran en el directorio `/home/ines/v1_1_9/gcc/results/speedup/base/perf`.
+### Resultados
+De cada benchmark he ejecutado una sola copia con 1, 2, 4, ... y 128 hilos de cada uno de los ejecutables que he obtenido de cada aplicación: gcc base, aocc y gcc peak (excepto 628.pop2_s, que solo ha creado un ejecutable base al compilar con gcc). Los únicos benchmarks que no he podido ejecutar de esta manera han sido el segundo de la aplicación 657.xz_s (que solo llega hasta 15 hilos) y el de 654.srom_s (solo llega hasta 8).
+
+Las ejecuciones han sido medidas con perf (por ahora solo ciclos e instrucciones) y los resultados se encuentran en los directorios `/home/ines/v1_1_9/gcc/results/speedup/base/perf`, `/home/ines/v1_1_9/gcc/results/speedup/peak/perf` y `/home/ines/v1_1_9/aocc/results/speedup/base/perf`.
---
+++ speedup_aocc_base.sh
@@ -0,0 +1,195 @@
+#!/bin/bash
+
+perf="perf stat -e cycles,instructions"
+
+xz_1="./xz cpu2006docs.tar.xz 6643 055ce243071129412e9dd0b3b69a21654033a9b723d874b2015c774fac1553d9713be561ca86f74e4f16f22e664fc17a79f30caa5ad2c04fbc447549c2810fae 1036078272 11795472 4"
+xz_2="./xz cld.tar.xz 1400 19cf30ae51eddcbefda78dd06014b4b96281456e078ca7c13e1c0c9e6aaea8dff3efb4ad6b0456697718cede6bd5454852652806a657bb56e07d61128434b474 536995164 539938872 8"
+
+cactuBSSN="./cactuBSSN spec_ref.par"
+
+lbm="./lbm 2000 reference.dat 0 0 200_200_260_ldc.of"
+
+wrf="./wrf"
+
+pop2="./pop2"
+
+imagick="./imagick -limit disk 0 refspeed_input.tga -resize 817% -rotate -2.76 -shave 540x375 -alpha remove -auto-level -contrast-stretch 1x1% -colorspace Lab -channel R -equalize +channel -colorspace sRGB -define histogram:unique-colors=false -adaptive-blur 0x5 -despeckle -auto-gamma -adaptive-sharpen 55 -enhance -brightness-contrast 10x10 -resize 30% refspeed_output.tga"
+
+nab="./nab 3j1n 20140317 220"
+
+fotonik3d="./fotonik3d"
+
+specrand="./specrand 1255432124 234923"
+
+DIR_RESULTS="/home/ines/v1_1_9/aocc/results/speedup/base/"
+
+DIR_AOCC="/home/ines/v1_1_9/aocc/"
+
+# 657.xz -----------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/xz_2.err
+> ${DIR_RESULTS}perf/xz_1.err
+
+cd ${DIR_AOCC}657
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/xz_1.err
+    echo "Ejecutando: taskset -c $cores $perf $xz_1 > ${DIR_RESULTS}outputs/cpu2006docs.tar-6643-4.out 2>> ${DIR_RESULTS}perf/xz_1.err"
+    taskset -c $cores $perf $xz_1 > ${DIR_RESULTS}outputs/cpu2006docs.tar-6643-4.out 2>> ${DIR_RESULTS}perf/xz_1.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/xz_1.err
+done
+
+for threads in 1 2 4 8 15; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/xz_2.err
+    echo "Ejecutando: taskset -c $cores $perf $xz_2 > ${DIR_RESULTS}outputs/cld.tar-1400-8.out 2>> ${DIR_RESULTS}perf/xz_2.err"
+    taskset -c $cores $perf $xz_2 > ${DIR_RESULTS}outputs/cld.tar-1400-8.out 2>> ${DIR_RESULTS}perf/xz_2.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/xz_2.err
+done
+
+# 603.bwaves ---------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/bwaves_1.err
+> ${DIR_RESULTS}perf/bwaves_2.err
+
+cd ${DIR_AOCC}603
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/bwaves_1.err
+    echo "Ejecutando: taskset -c $cores $perf ./bwaves_base bwaves_1 < bwaves_1.in > ${DIR_RESULTS}outputs/bwaves_1.out 2>> ${DIR_RESULTS}perf/bwaves_1.err"
+    taskset -c $cores $perf ./bwaves bwaves_1 < bwaves_1.in > ${DIR_RESULTS}outputs/bwaves_1.out 2>> ${DIR_RESULTS}perf/bwaves_1.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/bwaves_1.err
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/bwaves_2.err
+    echo "Ejecutando: taskset -c $cores $perf ./bwaves_base bwaves_2 < bwaves_2.in > ${DIR_RESULTS}outputs/bwaves_2.out 2>> ${DIR_RESULTS}perf/bwaves_2.err"
+    taskset -c $cores $perf ./bwaves bwaves_2 < bwaves_2.in > ${DIR_RESULTS}outputs/bwaves_2.out 2>> ${DIR_RESULTS}perf/bwaves_2.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/bwaves_2.err
+done
+
+# 607.cactuBSSN ------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/cactuBSSN.err
+
+cd ${DIR_AOCC}607
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/cactuBSSN.err
+    echo "Ejecutando: taskset -c $cores $perf $cactuBSSN > ${DIR_RESULTS}outputs/spec_ref.out 2>> ${DIR_RESULTS}perf/cactuBSSN.err"
+    taskset -c $cores $perf $cactuBSSN > ${DIR_RESULTS}outputs/spec_ref.out 2>> ${DIR_RESULTS}perf/cactuBSSN.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/cactuBSSN.err
+done
+
+# 619.lbm -----------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/lbm.err
+
+cd ${DIR_AOCC}619
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/lbm.err
+    echo "Ejecutando: taskset -c $cores $perf $lbm > ${DIR_RESULTS}outputs/lbm.out 2>> ${DIR_RESULTS}perf/lbm.err"
+    taskset -c $cores $perf $lbm > ${DIR_RESULTS}outputs/lbm.out 2>> ${DIR_RESULTS}perf/lbm.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/lbm.err
+done
+
+# 621.wrf -----------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/wrf.err
+
+cd ${DIR_AOCC}621
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/wrf.err
+    echo "Ejecutando: taskset -c $cores $perf $wrf > ${DIR_RESULTS}outputs/rsl.out.0000 2>> ${DIR_RESULTS}perf/wrf.err"
+    taskset -c $cores $perf $wrf > ${DIR_RESULTS}outputs/rsl.out.0000 2>> ${DIR_RESULTS}perf/wrf.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/wrf.err
+done
+
+# 628.pop2 -----------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/pop2.err
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cd ${DIR_AOCC}628/base/th$threads
+    cores=$(seq -s, 0 $((threads - 1)))
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/pop2.err
+    echo "Ejecutando: taskset -c $cores $perf $pop2 > ${DIR_RESULTS}outputs/pop2.out 2>> ${DIR_RESULTS}perf/pop2.err"
+    taskset -c $cores $perf $pop2 > ${DIR_RESULTS}outputs/pop2.out 2>> ${DIR_RESULTS}perf/pop2.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/pop2.err
+done
+
+# 638.imagick --------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/imagick.err
+
+cd ${DIR_AOCC}638
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/imagick.err
+    echo "Ejecutando: taskset -c $cores $perf $imagick > ${DIR_RESULTS}outputs/refspeed_convert.out 2>> ${DIR_RESULTS}perf/imagick.err"
+    taskset -c $cores $perf $imagick > ${DIR_RESULTS}outputs/refspeed_convert.out 2>> ${DIR_RESULTS}perf/imagick.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/imagick.err
+done
+
+# 644.nab ------------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/nab.err
+
+cd ${DIR_AOCC}644
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/nab.err
+    echo "Ejecutando: taskset -c $cores $perf $nab > ${DIR_RESULTS}outputs/3j1n.out 2>> ${DIR_RESULTS}perf/nab.err"
+    taskset -c $cores $perf $nab > ${DIR_RESULTS}outputs/3j1n.out 2>> ${DIR_RESULTS}perf/nab.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/nab.err
+done
+
+# 649.fotonick3d ------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/fotonik3d.err
+
+cd ${DIR_AOCC}649
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/fotonik3d.err
+    echo "Ejecutando: taskset -c $cores $perf $fotonik3d > ${DIR_RESULTS}outputs/fotonik3d.log 2>> ${DIR_RESULTS}perf/fotonik3d.err"
+    taskset -c $cores $perf $fotonik3d > ${DIR_RESULTS}outputs/fotonik3d.log 2>> ${DIR_RESULTS}perf/fotonik3d.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/fotonik3d.err
+done
+
+# 654.sroms -------------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/sroms.err
+
+cd ${DIR_AOCC}654
+
+for threads in 1 2 4 8; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/sroms.err
+    echo "Ejecutando: taskset -c $cores $perf ./sroms_base < ocean_benchmark3.in.x > ${DIR_RESULTS}outputs/ocean_benchmark3.log 2>> ${DIR_RESULTS}perf/sroms.err"
+    taskset -c $cores $perf ./sroms < ocean_benchmark3.in.x > ${DIR_RESULTS}outputs/ocean_benchmark3.log 2>> ${DIR_RESULTS}perf/sroms.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/sroms.err
+done
+
+# 996.specrand ----------------------------------------------------------------------------------------
+> ${DIR_RESULTS}perf/specrand.err
+
+cd ${DIR_AOCC}996
+
+for threads in 1 2 4 8 16 32 64 128; do
+    cores=$(seq -s, 0 $((threads - 1)))
+    export OMP_NUM_THREADS=$threads
+    echo "Threads: $threads" >> ${DIR_RESULTS}perf/specrand.err
+    echo "Ejecutando: taskset -c $cores $perf $specrand > ${DIR_RESULTS}outputs/rand.234923.out 2>> ${DIR_RESULTS}perf/specrand.err"
+    taskset -c $cores $perf $specrand > ${DIR_RESULTS}outputs/rand.234923.out 2>> ${DIR_RESULTS}perf/specrand.err
+    echo "---------------------------------" >> ${DIR_RESULTS}perf/specrand.err
+done
+
+cd ${DIR_AOCC}
